063dffcc407975dad4d91382b2b80efbf182ae60
arjun055 <nagarjunarjun711@gmail.com>
2024-11-18 14:40:18 +0530

    Resolved merge conflicts

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+import pinRoute from './routes/pins.js';
+import usersRoute from './routes/users.js';
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+mongoose
+.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("MongoDB connected");
+})
+.catch((err)=> console.log(err.reason))
+
+app.use("/api/pins",pinRoute);
+app.use("/api/users",usersRoute);
+
+app.listen(port,()=>{
+    console.log(`server listening on port ${port}...`);
+})
---
+++ backend/models/Pin.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const PinSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+    },
+    title:{
+        type: String,
+        required: true,
+        minlength: 3
+    },
+    desc:{
+        type: String,
+        required: true,
+        minlength: 3,
+    },
+    rating:{
+        type: Number,
+        required: true,
+        min:0,
+        max:5,
+    },
+    lat:{
+        type: Number,
+        required: true,
+    },
+    long:{
+        type: Number,
+        required: true,
+    }
+},{timestamps: true})
+
+
+export default mongoose.model('Pin',PinSchema);
